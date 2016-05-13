bb4019220c1e9c4db2f216491b2739f96305f04c
Walter Johnson <waltercj3@gmail.com>
2016-05-12 21:44:36 -0700

    up to 4.3.3

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+//var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 
 //var homepageController = function (req, res) {
 //  res.render('index', { title: '**Express**'})
 //};
 
 // GET home page.
-router.get('/', ctrlMain.index);
+//router.get('/', ctrlMain.index);
+
+// Locations pages
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+// Other pages
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
