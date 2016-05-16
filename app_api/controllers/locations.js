0684657d0075689b3d2f6f0fbebbef58df9e9c18
Walter Johnson <waltercj3@gmail.com>
2016-05-15 22:11:18 -0700

    into 6.2.2

---
+++ app_api/controllers/locations.js
@@ -0,0 +1,6 @@
+
+module.exports.locationsListByDistance = function (req, res) { };
+module.exports.locationsCreate = function (req, res) { };
+module.exports.locationsReadOne = function (req, res) { };
+module.exports.locationsUpdateOne = function (req, res) { };
+module.exports.locationsDeleteOne = function (req, res) { };
