9daad782e792fb4c6d5e957c07eacfaa874fad37
Walter Johnson <waltercj3@gmail.com>
2016-05-16 22:22:29 -0700

    through 6.3.2

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -8,15 +8,38 @@ var sendJsonResponse = function(res, status, content) {
 module.exports.locationsListByDistance = function (req, res) {
   sendJsonResponse(res, 200, {"status" : "success"});
 };
+
 module.exports.locationsCreate = function (req, res) {
   sendJsonResponse(res, 200, {"status" : "success"});
 };
-module.exports.locationsReadOne = function (req, res) {
-  sendJsonResponse(res, 200, {"status" : "success"});
+
+module.exports.locationsReadOne = function(req, res) {
+  if (req.params && req.params.locationid) {
+    Loc
+      .findById(req.params.locationid)
+      .exec(function(err, location) {
+        if (!location) {
+          sendJsonResponse(res, 404, {
+            "message": "locationid not found"
+          });
+          return;
+        } else if (err) {
+          sendJsonResponse(res, 404, err);
+          return;
+        }
+        sendJsonResponse(res, 200, location);
+      });
+  } else {
+    sendJsonResponse(res, 404, {
+      "message": "No locationid in request"
+    });
+  }
 };
+
 module.exports.locationsUpdateOne = function (req, res) {
   sendJsonResponse(res, 200, {"status" : "success"});
 };
+
 module.exports.locationsDeleteOne = function (req, res) {
   sendJsonResponse(res, 200, {"status" : "success"});
 };
--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -8,12 +8,60 @@ var sendJsonResponse = function(res, status, content) {
 module.exports.reviewsCreate = function (req, res) {
   sendJsonResponse(res, 200, {"status" : "success"});
 };
-module.exports.reviewsReadOne = function (req, res) {
-  sendJsonResponse(res, 200, {"status" : "success"});
+
+module.exports.reviewsReadOne = function(req, res) {
+  //console.log("Getting single review");
+  if (req.params && req.params.locationid && req.params.reviewid) {
+    Loc
+      .findById(req.params.locationid)
+      .select('name reviews')
+      .exec(function(err, location) {
+        //console.log("location:" + location);
+        var response, review;
+        if (!location) {
+          sendJsonResponse(res, 404, {
+            "message": "locationid not found"
+          });
+          return;
+        } else if (err) {
+          sendJsonResponse(res, 400, err);
+          return;
+        }
+        if (location.reviews && location.reviews.length > 0) {
+          //console.log("location.reviews.id:" + location.reviews.id);
+          review = location.reviews.id(req.params.reviewid);
+          //console.log("review:" + review);
+          if (!review) {
+            sendJsonResponse(res, 404, {
+              "message": "reviewid not found"
+            });
+          } else {
+            response = {
+              location : {
+                name : location.name,
+                id : req.params.locationid
+              },
+              review : review
+            };
+            sendJsonResponse(res, 200, response);
+          }
+        } else {
+          sendJsonResponse(res, 404, {
+            "message": "No reviews found"
+          });
+        }
+      });
+  } else {
+    sendJsonResponse(res, 404, {
+      "message": "No locationid in request"
+    });
+  }
 };
+
 module.exports.reviewsUpdateOne = function (req, res) {
   sendJsonResponse(res, 200, {"status" : "success"});
 };
+
 module.exports.reviewsDeleteOne = function (req, res) {
   sendJsonResponse(res, 200, {"status" : "success"});
 };
