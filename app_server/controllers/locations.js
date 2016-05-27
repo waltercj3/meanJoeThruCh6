a7c4dd6059e8ef8e85cd2a29aea09fbf9b9de2eb
waltercs296 <walter.johnson.0301@mail.linnbenton.edu>
2016-05-27 23:12:57 +0000

    through 7.3.5

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -77,46 +77,15 @@ module.exports.homelist = function(req, res){
 };
 
 // GET 'Location info' page
-var renderDetailPage = function(req, res){
+var renderDetailPage = function (req, res, locDetail) {
   res.render('location-info', {
-    title: 'Starcups',
-    pageHeader: {title: 'Starcups'},
+    title: locDetail.name,
+    pageHeader: {title: locDetail.name},
     sidebar: {
       context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
       callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
     },
-    location: {
-      name: 'Starcups',
-      address: '125 High Street, Reading, RG6 1PS',
-      rating: 3,
-      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-      coords: {lat: 51.455041, lng: -0.9690884},
-      openingTimes: [{
-        days: 'Monday - Friday',
-        opening: '7:00am',
-        closing: '7:00pm',
-        closed: false
-      },{
-        days: 'Saturday',
-        opening: '8:00am',
-        closing: '5:00pm',
-        closed: false
-      },{
-        days: 'Sunday',
-        closed: true
-      }],
-      reviews: [{
-        author: 'Simon Holmes',
-        rating: 5,
-        timestamp: '16 July 2013',
-        reviewText: 'What a great place. I can\'t say enough good things about it.'
-      },{
-        author: 'Charlie Chaplin',
-        rating: 3,
-        timestamp: '16 June 2013',
-        reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
-      }]
-    }
+    location: locDetail
   });
 };
 module.exports.locationInfo = function(req, res){
@@ -130,7 +99,12 @@ module.exports.locationInfo = function(req, res){
   request(
     requestOptions,
     function(err, response, body) {
-      renderDetailPage(req, res);
+      var data = body;
+      data.coords = {
+        lng : body.coords[0],
+        lat : body.coords[1]
+      };
+      renderDetailPage(req, res, data);
     }
   );
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -13,7 +13,7 @@ var ctrlOthers = require('../controllers/others');
 
 // Locations pages
 router.get('/', ctrlLocations.homelist);
-router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/:locationid', ctrlLocations.locationInfo);
 router.get('/location/review/new', ctrlLocations.addReview);
 
 // Other pages
